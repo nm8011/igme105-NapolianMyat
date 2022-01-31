e1a57b0a6fbf6367be6dc339427c0f67c445a775
nm8011 <nm8011@IGM2025-21>
2022-01-31 13:24:48 -0500

    fin

--- Homework/MyatN-HW1/MyatN-HW1/Program.cs
+++ Homework/MyatN-HW1/MyatN-HW1/Program.cs
@@ -19,17 +19,25 @@ namespace MyatN_HW1
             //Variables
             string name;    //save user name
             string playGame;    //whether to play game or not
+            //Create a constant value for the number of steps required to get from the driveway to the house.
+            //(You can select your own positive integer value as the total number of steps as long as it is less than 100).
+            const int numOfSteps = 50;
+            int userAnsNum;
+            int calcNum; //calculations to show the user how far they would have walked past the house and notify them you stopped at the door.    
 
+            //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
             //Introduction
             //•	Provide the name of the game
             //•	Provide instructions for how to play the game
+            //leave it black
+            Console.BackgroundColor = ConsoleColor.Black;
             Console.WriteLine("Welcome to Morg's Diabetic Sugar Adventure");
             Console.WriteLine("__________________________________________\n");
 
 
             // User starts here – ask for basic information such as user name.
             //•	Ask the user for their name, read it in and save it (It is used multiple times throughout the game)
-            Console.WriteLine("Please enter your character name: ");
+            Console.Write("Please enter your character name: ");
             name = Console.ReadLine();
 
             //Greetings
@@ -37,24 +45,25 @@ namespace MyatN_HW1
             //change font color
             Console.ForegroundColor = ConsoleColor.DarkYellow;
             Console.WriteLine("Nice to meet you, " + name + " I hope you are prepared to die in a sugar coma!\n");
-            //change color back
-            Console.ForegroundColor = ConsoleColor.White;
 
             // Section to setup initial storyline
             //•	Provide instructions for how to play the game
-            Console.ForegroundColor = ConsoleColor.DarkBlue;
             Console.WriteLine(" Morg's Diabetic Sugar Adventure is an interactive text based game.\nDuring the game you will" +
                 " be asked for decisions and to enter input.\nYou will be provided with all that you need to know as you play " +
                 "the game\nand only need a keyboard to play.  Enjoy the adventure!\n");
+            Console.ForegroundColor = ConsoleColor.White;
+            //change color back
 
             //•	Add a section inquiring if the user wants to start playing the game and asking for a Y/ N response.
             Console.WriteLine("Are you ready to play (Y/N)?");
+            Console.Write(">");
+            Console.ForegroundColor = ConsoleColor.White;
             playGame = Console.ReadLine().Trim().ToUpper(); //trim/toupper senitize userinput
 
             //In case player enters yes or no instead of y or n
-            if(playGame == "YES" || playGame == "NO")
+            if (playGame == "YES" || playGame == "NO")
             {
-                Console.WriteLine("*sigh*.... we asked for a 'Y' or an 'N'");
+                Console.WriteLine("\n*sigh*.... we asked for a 'Y' or an 'N'");
                 playGame = playGame.Substring(0, 1);
 
             }
@@ -68,7 +77,30 @@ namespace MyatN_HW1
             }
             else
             {
+                //Ask the user for how many steps to move.
+                Console.WriteLine("\nHow many steps are you going to move?");
+                //Read the user step input in and convert it to an integer
+                userAnsNum = int.Parse(Console.ReadLine());
 
+                //Calculations for over and under the number of required steps created and used in output. 
+                calcNum = Math.Abs(numOfSteps - userAnsNum);
+
+
+                //user input is greater
+                if (userAnsNum > numOfSteps)
+                {
+                    Console.WriteLine("You would have walked past the door by {0} steps. You have stopped at the door.", calcNum);
+                }
+                //user input less than
+                else if (userAnsNum < numOfSteps)
+                {
+                    Console.WriteLine("You are short of {0} steps. I have increased your steps. Now you are at the door.", calcNum);
+                }
+                //userinput == const
+                else
+                {
+                    Console.WriteLine("You are right on the mark! You have arrived at the door.");
+                }
             }
 
 
