96663131662d2080c0035601ea20af4e226a7d92
nm8011 <nm8011@rit.edu>
2022-01-31 10:45:35 -0500

    ok

---
+++ Homework/MyatN-HW1/MyatN-HW1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+/// <summary>
+/// Napolian Myat
+/// Project: Homework1
+/// Date: 01/31/2022
+/// Purpose: For this homework, we will begin creating our Text Adventure using:
+/// •	Use assignment statements
+/// •	Use simple input & output
+/// •	expressions and casting
+/// •	the strings class
+
+/// </summary>
+namespace MyatN_HW1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Introduction
+            Console.WriteLine("Welcome to Morg's Diabetic Sugar Adventure");
+            // User starts here – ask for basic information such as user name.
+            // Section to setup initial storyline
+            // What is the flow?
+            // path to front door
+            // challenge to get in front door
+            // what do you see when get in the door?
+            // User finds the dungeon door and escapes with the Hershey Candy Golden Globes
+
+        }
+    }
+}
