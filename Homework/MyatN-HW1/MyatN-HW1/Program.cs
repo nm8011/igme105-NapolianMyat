70d0e2cc45cc1b3b78fb84266bf478af8da9ff0a
nm8011 <nm8011@IGM2025-21>
2022-02-21 18:11:19 -0500

    ok

--- Homework/MyatN-HW1/MyatN-HW1/Program.cs
+++ Homework/MyatN-HW1/MyatN-HW1/Program.cs
@@ -1,135 +1,125 @@
 ﻿using System;
-/* Name: Geoff, Xavier, and Napolian
- * Project: Trouble Game
- * Date: 1/21/2022
- * Purpose: To create a console version of the Trouble board game
- * Modifications: (PE03, 1/21/2022)
- */
-namespace PETrouble
+/// <summary>
+/// Napolian Myat
+/// Project: Homework2
+/// Date: 01/31/2022
+/// Modified: 02/21/2022
+/// Purpose: Extension of hw1
+/// For this homework, we will begin creating our Text Adventure using:
+///•	Random class
+///•	string and math class methods
+///•	If Statements and Relational\ Boolean Operators
+///•	Nested If Statements
+///•	Switch Statements
+///•	Loops
+/// </summary>
+namespace MyatN_HW1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            //Attributes - Players 
+            //Variables
+            string name;    //save user name
+            string playGame;    //whether to play game or not
+            //Create a constant value for the number of steps required to get from the driveway to the house.
+            //(You can select your own positive integer value as the total number of steps as long as it is less than 100).
+            const int numOfSteps = 50;
+            int userAnsNum;
+            int calcNum; //calculations to show the user how far they would have walked past the house and notify them you stopped at the door.    
 
-            // Real Person
-            /*
-             * Pieces - # of pieces
-             * 
-             */
-            //const int numOfPlayers;
+            //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+            //Introduction
+            //•	Provide the name of the game
+            //•	Provide instructions for how to play the game
+            //leave it black
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.WriteLine("Welcome to Morg's Diabetic Sugar Adventure");
+            Console.WriteLine("__________________________________________\n");
 
-            int PlayerPiece1;
-            int PlayerPiece2;
-            int PlayerPiece3;
-            int PlayerPiece4;
 
-            string playerAgeRead;
-            string PieceColor;
-            string PlayerName;
+            // User starts here – ask for basic information such as user name.
+            //•	Ask the user for their name, read it in and save it (It is used multiple times throughout the game)
+            Console.Write("Please enter your character name: ");
+            name = Console.ReadLine();
 
-            // AI Person - Variables
-            int AIPlayerPiece1;
-            int AIPlayerPiece2;
-            int AIPlayerPiece3;
-            int AIPlayerPiece4;
+            //Greetings
+            //•	Provide the initial narration, using the name provided by the user.
+            //change font color
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("Nice to meet you, " + name + " I hope you are prepared to die in a sugar coma!\n");
 
-            string AIName;
-            string AIColor;
-            /* Pieces
-             * Piece Color
-         * AIName
-         */
+            // Section to setup initial storyline
+            //•	Provide instructions for how to play the game
+            Console.WriteLine(" Morg's Diabetic Sugar Adventure is an interactive text based game.\nDuring the game you will" +
+                " be asked for decisions and to enter input.\nYou will be provided with all that you need to know as you play " +
+                "the game\nand only need a keyboard to play.  Enjoy the adventure!\n");
+            Console.ForegroundColor = ConsoleColor.White;
+            //change color back
 
-            //Board
-            /* Board Locations
-             * Board Length
-             */
-            int PlayerPosition = 0;
-            int AIPosition = 14;
-            // Move pieces
-            // Rolling Dice
-            // Reset (i.e. send a piece home - poor opponent)
-            // Setup game
-            // Game Status (who won/lost)
-            // Turn order (who's up!)
+            //•	Add a section inquiring if the user wants to start playing the game and asking for a Y/ N response.
+            Console.WriteLine("Are you ready to play (Y/N)?");
+            Console.Write(">");
+            Console.ForegroundColor = ConsoleColor.White;
+            playGame = Console.ReadLine().Trim().ToUpper(); //trim/toupper senitize userinput
 
-            //ask player name
-            Console.Write("Welcome player, to our horrible game of (not) Trouble, by what name can we call you? :");
-            PlayerName = Console.ReadLine();
-            Console.WriteLine();
-
-            //ask player age
-            Console.Write("What is your age? ");
-            playerAgeRead = Console.ReadLine();
-            Console.WriteLine();
-
-            //checking to see if age is a real valid number
-            int ageNum;
-
-            bool success = int.TryParse(playerAgeRead, out ageNum);
-
-            if (!success)
+            //In case player enters yes or no instead of y or n
+            if (playGame == "YES" || playGame == "NO")
             {
-                Console.WriteLine("You didn't enter a valid age number. So you are now 2 years old. ha. You are baby. Your actions have consequences.");
-                ageNum = 2;
+                Console.WriteLine("\n*sigh*.... we asked for a 'Y' or an 'N'");
+                if (playGame == "NO")
+                {
+                    Console.WriteLine("\nYou have decided you are not ready to play the game.\nCome again when you are ready. :)");
+                }
+                playGame = playGame.Substring(0, 1);
+
+
+            }
+            //test
+            //Console.WriteLine(playGame);
+
+            if (playGame != "Y")
+            {
+                //They gave us anything BUT Y
+                return;
             }
             else
             {
-                ageNum = int.Parse(playerAgeRead);
+                //Ask the user for how many steps to move.
+                Console.WriteLine("\nHow many steps are you going to move?");
+                //Read the user step input in and convert it to an integer
+                userAnsNum = int.Parse(Console.ReadLine());
+
+                //Calculations for over and under the number of required steps created and used in output. 
+                calcNum = Math.Abs(numOfSteps - userAnsNum);
+
+
+                //user input is greater
+                if (userAnsNum > numOfSteps)
+                {
+                    Console.WriteLine("You would have walked past the door by {0} steps. You have stopped at the door.", calcNum);
+                }
+                //user input less than
+                else if (userAnsNum < numOfSteps)
+                {
+                    Console.WriteLine("You are short of {0} steps. I have increased your steps. Now you are at the door.", calcNum);
+                }
+                //userinput == const
+                else
+                {
+                    Console.WriteLine("You are right on the mark! You have arrived at the door.");
+                }
             }
 
-            Console.WriteLine();
-            //ask player color
-            Console.Write("Thank you {0}({1}),Which color do you wish to be? (B)lue, (R)ed, or (Y)ellow:", PlayerName, ageNum);
-            PieceColor = Console.ReadLine();
-
-            Console.WriteLine();
-
-            //Welcome
-            Console.WriteLine("\t\t\tWelcome {0}({1}) to the Trouble Pop-O-Matic Game!", PlayerName, ageNum);
-            Console.WriteLine("\t\t ----------------------------------------------------- \n");
-
-            //Rules
-            Console.WriteLine("Rules:");
-            Console.WriteLine(" ----------------------------------------------------------------------------------");
-
-            Console.WriteLine("Trouble is easy to play. \n" +
-                "\t -Players will take turns to roll a die.\n  \t -Roll a 6 to start a piece on the board.\n \t -Get all 4 pieces around before any other player. \n \t -Land on another player piece and send them back to start. \n ");
-
-            Console.WriteLine(" ----------------------------------------------------------------------------------");
-
-            //wishing gl
-            Console.WriteLine("\t\t\t Good Luck! \n");
 
 
 
-            //ask player ai's name
-            Console.Write("What name do you wish to grant thy formidable enemy(AI)? :");
-            AIName = Console.ReadLine();
-            //ai automatically granted the color black
-            AIColor = "Black";
-
-
-
-
-
-
-
-            //Stats
-            Console.WriteLine("--------------------------------------------------------------------------------");
-            Console.WriteLine("|\tPlayerName: " + PlayerName + "(" + ageNum + ")" + " \tPiece Color: " + PieceColor + "\t\t Player position: " + PlayerPosition + "\t|");
-            Console.WriteLine("|\tAIName: " + AIName + " \t\t Piece Color: " + AIColor + "\t AI position: " + AIPosition + "\t|");
-            Console.WriteLine("--------------------------------------------------------------------------------");
-
-            //get date
-            DateTime now = DateTime.Now;
-
-            //print time/date (formatted)
-            Console.WriteLine("The current date and time is: " + now.ToString("MM/dd/yyyy hh:mm tt"));
-
-
+            Console.ForegroundColor = ConsoleColor.White;
+            // What is the flow?
+            // path to front door
+            // challenge to get in front door
+            // what do you see when get in the door?
+            // User finds the dungeon door and escapes with the Hershey Candy Golden Globes
 
         }
     }
