2d997d74a2bb9bca01ad0f40f1baedec18b69144
nm8011 <nm8011@IGM2025-21>
2022-02-25 21:07:57 -0500

    almost

--- Homework/MyatN-HW1/MyatN-HW1/Program.cs
+++ Homework/MyatN-HW1/MyatN-HW1/Program.cs
@@ -59,6 +59,13 @@ namespace MyatN_HW1
             //Ask if player wants to continue or quit
             Quit();
 
+            UpperFloor();
+
+            DungeonFloors();
+
+            
+
+
 
 
 
@@ -87,7 +94,7 @@ namespace MyatN_HW1
         {
             // Section to setup initial storyline
             //•	Provide instructions for how to play the game
-            Console.WriteLine("\tMystical Grooto is an interactive text based game.\nDuring the game you will" +
+            Console.WriteLine("\tMystical Grotto is an interactive text based game.\nDuring the game you will" +
                 " be asked for decisions and to enter input.\nYou will be provided with all that you need to know as you play " +
                 "the game\nand will only require a keyboard to play. Enjoy the adventure!\n");
             Console.ForegroundColor = ConsoleColor.White;//change color back
@@ -127,7 +134,7 @@ namespace MyatN_HW1
             //•	Provide the initial narration, using the name provided by the user.
             //change font color
             Console.ForegroundColor = ConsoleColor.DarkYellow;
-            Console.WriteLine("Nice to meet you, {0} I hope you are prepared to die in a sugar coma!\n", name);
+            Console.WriteLine("Nice to meet you, {0} I hope you are prepared to perish in a dark damp lair!\n", name);
             return name;
         }
         /// <summary>
@@ -276,23 +283,28 @@ namespace MyatN_HW1
             string response;
 
             Console.WriteLine("Do you wish to quit or continue? Enter 'Q' to quit or anything else to continue");
-            response = Console.ReadLine().Trim().Substring(0, 1).ToUpper();
-            if (response != "Q")
+            response = Console.ReadLine().Trim().Substring(0).ToUpper(); //substring 0 allows for null
+            if (string.IsNullOrEmpty(response)) //if user input is null meaning they press "Enter"
             {
-                //code continues
                 Console.WriteLine("You've decided to continue. Good Luck Adventurer!");
+
             }
-            //doesnt work?
-            //else if (string.IsNullOrEmpty(response))
-            //{
-            //    quit = false;
-            //    Console.WriteLine("You've decided to continue. Good Luck Adventurer!");
-            //}
             else
             {
-                Console.WriteLine("Your persistence is pathetic. You have decided to quit adventuring.");
-                Environment.Exit(0);
+                response = response.Substring(0, 1).ToUpper().Trim();//user senitize
+                if (response != "Q")
+                {
+                    //code continues
+                    Console.WriteLine("You've decided to continue. Good Luck Adventurer!");
+                }
+                else
+                {
+                    Console.WriteLine("Your persistence is pathetic. You have decided to quit adventuring.");
+                    Environment.Exit(0);
+                }
             }
+           
+            
 
         }
 
@@ -322,64 +334,90 @@ namespace MyatN_HW1
         public static void UpperFloor()
         {
             string choice;
-            bool valid = false;
-            string stringColor = StrClass(2);
+            bool exit = false;
+        
+            string stringColor = StrClass(3); //find 3rd word
             Console.WriteLine("You entered through the door. You are at the corridor glowing a bright {0} color.", stringColor);
-            Console.WriteLine("You see 5 rooms within your vicinity. You should probably check each of them out\nThey are:\n\t1 The Kitchen\n\t" +
-                "2 The BedRoom\n\t3 The LivingRoom\n\t4 The Library\n\t5 The GuestRoom");
             do
             {
+                Console.WriteLine("\nYou see 5 rooms within your vicinity. You should probably check each of them out\nThey are:\n\t1 The Kitchen\n\t" +
+                    "2 The BedRoom\n\t3 The LivingRoom\n\t4 The Library\n\t5 The GuestRoom.\nWhich one do you wish to visit?");
                 choice = Console.ReadLine().Trim();
                 switch (choice)
                 {
                     case "1":
                         {
-                            Console.WriteLine("You notice in the kitchen some items that could be essential to your adventure. " +
-                                "\nThe kitchen is very bright and lively with a fridge mounted right in the middle. You looked inside the fridge and found\n" +
-                                "an abundant of food and water. You also located a cutting board nearby"); //get shield & food, water
-                            valid = true;
+                            Console.WriteLine("\nYou notice in the kitchen some items that could be essential to your adventure. " +
+                                "\nThe kitchen is very bright and lively with a fridge mounted right in the middle. \nYou looked inside the fridge and found" +
+                                "an abundant of food and water. \nYou also located a cutting board nearby"); //get shield & food, water
+                            exit = false;
                             break;
                         }
                     case "2":
                         {
-                            Console.WriteLine("You entered the Bed Room. The living room has a TV that is playing some channels\n" +
+                            Console.WriteLine("\nYou entered the Bed Room. The living room has a TV that is playing some channels\n" +
                                 "you are not familiar with, as well as jazz music playing in the background. There is a set of \n" +
                                 "headphones connected to a disc player."); //will need maybe to block noise(dmg from sound), less dmg over time so without, the player
-                            //will lose a certain amount of hp each round or step or whatev(not decided)
+                                                                           //will lose a certain amount of hp each round or step or whatev(not decided)
+                            exit = false;
+
                             break;
                         }
                     case "3":
                         {
-                            Console.WriteLine("The Livingroom is neatly organized, there is a sweet scent in the air. Hey theres some marshmellows attach to a fire poker" +
-                                "\nAs you tried to pick the marshmellows, you got a cut. Luckily you did not lose any health points. You observed that the \n" +
-                                "fire poker is oddly sharp. That might prove handy. "); //weapon, fight against minions and boss later, otherwise fistfights dmg will be halved.
+                            Console.WriteLine("\nThe Living room is neatly organized, there is a sweet scent in the air. Hey theres some \nmarshmellows attach to a fire poker" +
+                                "As you tried to pick the marshmellows, you got a cut. Luckily you \ndid not lose any health points. You observed that the " +
+                                "fire poker is oddly sharp. \nThat might prove handy. "); //weapon, fight against minions and boss later, otherwise fistfights dmg will be halved.
+                            exit = false;
                             break;
                         }
                     case "4":
                         {
-                            Console.WriteLine("The Library has a ladder leading up to the top of the shelves. As you checked out the area, you notice a bunch of potions.\n" +
+                            Console.WriteLine("\nThe Library has a ladder leading up to the top of the shelves. As you checked out \nthe area, you notice a bunch of potions." +
                                 "Theres a note nearby. You read it:" +
                                 "\n\"To whomever it concerns,\n\t" +
                                 "Take great discretion towards what I am about to tell you. The maze you will face will have great amount of danger. I have prepared\n" +
-                                "you some potions for your adventure. However PLEASE PICK ONLY ONE potions for consumption. Anymore will kill an ordinary man.");
+                                "you some potions for your adventure. However PLEASE PICK ONLY ONE potions for consumption. Anymore will kill an ordinary man.\"\n");
                             //potions will be, 2x dmg increased, health over time/rounds, TBD later
+
+                            Console.WriteLine("You got down and noticed a sculpture nearby a pillar. Based on your recollection of how most hidden rooms are, \n" +
+                                "it probably hides a secret passage. You yanked the scupture. Mechanism start to spin. The room rumbles and revealed\n" +
+                                "a hidden door. Its the door to the basement.");
+                            Console.WriteLine("\nThe basement is emmitting a murderous hue. You entered and the door behind you is \nshut tight with a loud slam. You tried to backout but no amount of hammering will open this door.\n" +
+                               "\nYou must persist forward."); //introduce basement dungeon floors
+                            exit = true;
                             break;
+
                         }
                     case "5":
                         {
-                            Console.WriteLine("The GuestRoom has an allocated bed. You search all over the room for items that will assist your journey. You checked under the bed \n" +
-                                "and found a secret suitcase. You need a key to open it. Good thing a key is right besides the nightstand. You used it to open the case. Perfect match!\n" +
-                                "You checked the contents. There is a sturdy but surprisely nimble cloth inside. Hey! That might make you more resiliant.");//armour, maybe add some options to choose from
-                            //can halves dmg taken?
+                            Console.WriteLine("\nThe Guest Room has an allocated bed. You search all over the room for items that will \nassist your journey. You checked under the bed " +
+                                "and found a secret suitcase. You need a key to open it. Good thing \na key is right besides the nightstand. You used it to open the case. \nPerfect match! " +
+                                "You checked the contents. There is a sturdy but surprisely nimble cloth\ninside. Hey! That might make you more resiliant.");//armour, maybe add some options to choose from
+                                                                                                                                                            //can halves dmg taken?
+                            exit = false;
+
+                            break;
+                        }
+                    default://if not valid input prompt again
+                        {
+                            Console.WriteLine("\nOops! You have not picked an available option. Please pick again.");
+                            //Console.WriteLine("You see 6 rooms within your vicinity. You should probably check each of them out\nThey are:\n\t1 The Kitchen\n\t" +
+                            //        "2 The Bed Room\n\t3 The Living Room\n\t4 The Library\n\t5 The Guest Room\n\t6 The Basement\n\nWhich one do you wish to visit?");
+                            //choice = Console.ReadLine().Trim();
+                            exit = false;
                             break;
                         }
                 }
             }
-            while (valid == false);
-         
+            while (exit == false);
+
         }
 
-        public static void 
+        public static void DungeonFloors()
+        {
+
+        }
         //Hopefully works
         //NO CODE OUTSIDE HERE
     }
