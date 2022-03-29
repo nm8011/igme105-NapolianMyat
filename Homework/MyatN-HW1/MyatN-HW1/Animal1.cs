bc1b4ab20da9522ffc83a57724360e0d9efeb2a1
nm8011 <nm8011@IGM2025-22>
2022-03-29 16:53:41 -0400

    ok

--- Homework/MyatN-HW1/MyatN-HW1/Animal1.cs
+++ Homework/MyatN-HW1/MyatN-HW1/Animal1.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using System.Text;
 /// <summary>
 /// Napolian Myat
-/// Project: Homework3
+/// Project: Homework4
 /// Date: 01/31/2022
-/// Modified: 03/02/2022
+/// Modified: 03/29/2022
 /// Purpose: Extension of hw1
 /// For this homework, we will begin creating our Text Adventure using:
 ///•	Random class
@@ -17,6 +17,9 @@ using System.Text;
 ///•	Method Overloading / Variable Scope
 ///•	Method parameters returning Values
 ///•	Classes
+///•	Class Properties
+///•	Arrays
+///•	Lists and Dictionaries
 /// </summary>
 namespace MyatN_HW1
 {
