5cd42e5cd97e4e99fc6614bf29d453953a75371f
nm8011 <nm8011@rit.edu>
2022-04-08 10:06:49 -0400

    half there

---
+++ NM_PE16/NM_PE16/Ship.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+/// <summary>
+/// Napolian Myat
+/// 04/08/2022
+/// Professor Warren
+/// Project: PE16
+/// Purpose: We are going to examine the use of polymorphism. 
+/// A Ship class will be defined. This class will be used as 
+/// the parent of the Warship and Transport classes. We will 
+/// then try to use polymorphism with some methods.
+/// </summary>
+namespace NM_PE16
+{
+    class Ship
+    {
+        //VARIABLES
+        //2.	Make all three attributes private.
+        private string name;
+        private int weight;
+        private int maxSpeed;
+
+        //PROPERTIES
+        //3. Provide properties for each attribute that allows a child class to get
+        //their values. Do not allow changes to the attributes through the properties.
+        public string Name { get { return name; }}
+        public int Weight { get { return weight; } }
+        public int MaxSpeed { get { return maxSpeed; } }
+
+
+
+        //CONSTRUCTOR
+        /// <summary>
+        /// 1. Create a Ship class (this will be the parent) with a parameterized constructor 
+        /// that takes a ship name, weight (in tons), and maximum speed as parameters.
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="weight"></param>
+        /// <param name="maxSpeed"></param>
+        public Ship(string name, int weight, int maxSpeed)
+        {
+            this.name = name;
+            this.weight = weight;
+            this.maxSpeed = maxSpeed;
+        }
+        /// <summary>
+        /// 4.	Override the ToString method to return a String containing the ship’s information.
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            string text = string.Format("\nShip Details" +
+                "\n\tShip Name: {0}" +
+                "\n\tWeight: {1}" +
+                "\n\tMax Speed: {2}"
+                , name, weight, maxSpeed);
+            return text;
+        }
+    }
+}
---
+++ NM_PE16/NM_PE16/Warship.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+/// <summary>
+/// Napolian Myat
+/// 04/08/2022
+/// Professor Warren
+/// Project: PE16
+/// Purpose: We are going to examine the use of polymorphism. 
+/// A Ship class will be defined. This class will be used as 
+/// the parent of the Warship and Transport classes. We will 
+/// then try to use polymorphism with some methods.
+/// </summary>
+namespace NM_PE16
+{
+    /// <summary>
+    /// 1.	Create a separate class Warship class that inherits from the Ship class.
+    /// </summary>
+    class Warship : Ship
+    {
+        //VARIABLE
+        //2.Add a number of guns attribute to the Warship class.
+        private int numOfGun;
+
+        //PROPERTY
+
+        public int NumOfGun { get { return numOfGun; } }
+
+        //CONSTRUCTOR
+        /// <summary>
+        /// 3.	Provide a parameterized constructor that has the three attributes for 
+        /// the parent class and an addition one for the number of guns on the ship.
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="weight"></param>
+        /// <param name="maxSpeed"></param>
+        /// <param name="numOfGun"></param>
+        public Warship(string name, int weight, int maxSpeed, int numOfGun):base(name, weight, maxSpeed)
+        {
+            this.numOfGun = numOfGun;
+        }
+    }
+}
