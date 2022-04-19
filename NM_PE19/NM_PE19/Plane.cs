474e5d3766ceb3dcb6f03382fe9a5424071cd60d
nm8011 <nm8011@rit.edu>
2022-04-18 23:02:12 -0400

    finish

---
+++ NM_PE19/NM_PE19/Plane.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+namespace NM_PE19
+{
+    class Plane
+
+    {
+        //VARIABLE
+        private string fileName;
+        private string planeName;
+        private string country;
+        private char turnMode;
+        private char attackMode;
+        private double cost;
+        private int maxDamage;
+
+        public Plane(string fileName, string planeName, string country, 
+            char turnMode, char attackMode, double cost, int maxDamage)
+        {
+            this.fileName = fileName;
+            this.planeName = planeName;
+            this.country = country;
+            this.turnMode = turnMode;
+            this.attackMode = attackMode;
+            this.cost = cost;
+            this.maxDamage = maxDamage;
+        }
+
+        /// <summary>
+        /// 	One should take all of the data for a single plane as parameters,
+        /// as well as a file name, and write it to that file.
+        /// </summary>
+        public void WriteBinary()
+        {
+            try
+            {
+                //Create stream and binarywriter
+                Stream plane = File.OpenWrite(fileName);
+                BinaryWriter output = new BinaryWriter(plane);
+
+                //Hardcode data
+                output.Write(planeName);
+                output.Write(country);
+                output.Write(turnMode);
+                output.Write(attackMode);
+                output.Write(cost);
+                output.Write(maxDamage);
+
+                output.Close();//closing file
+            }
+            catch (IOException x)
+            {
+                Console.WriteLine("Error creating {0}: ", fileName + x.Message);
+            }
+        }
+        /// <summary>
+        /// 	The other should take a file name, read the file and print the data out.
+        /// </summary>
+        public void ReadBinary()
+        {
+            try
+            {
+                //Create binaryreader
+                BinaryReader input = new BinaryReader(File.OpenRead(fileName));
+
+                ///•	Select appropriate data types for the data being represented.
+                ///The idea is to take up as little space as possible.  If you store 
+                ///all the data as Strings, you will NOT get any credit.
+                string strData = input.ReadString();
+                Console.WriteLine("Name: " +strData);
+
+                strData = input.ReadString();
+                Console.WriteLine("Country: " +strData);
+
+                char charData = input.ReadChar();
+                Console.WriteLine("Turn Mode: " + charData);
+
+                charData = input.ReadChar();
+                Console.WriteLine("Attack Mode: " + charData);
+
+                double dblData = input.ReadDouble();
+                Console.WriteLine("Cost: "+dblData);
+
+                int intData = input.ReadInt32();
+                Console.WriteLine("Max Damage: "+intData);
+
+                //close file
+                input.Close();
+            }
+            catch(IOException x)
+            {
+                Console.WriteLine("Error reading {0}: ", fileName + x.Message);
+            }
+
+        }
+    }
+}
---
+++ NM_PE19/NM_PE19/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace NM_PE19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //•	Get a listing of all files in a specific directory. (Hint: use one of the classes we discussed at the start of lecture.)
+            Console.WriteLine("Current Directory: " + Directory.GetCurrentDirectory());
+            string[] files = Directory.GetFiles(".");
+            foreach (string file in files)
+            {
+                Console.WriteLine("File name: " + file);
+            }
+            Console.WriteLine();
+
+            //CREATE FILE READWRITE FILE - DISPLAY DATA
+            //•	Read only the “plane” files back in and display the data for each one.
+            Plane fokkerPlane = new Plane("fokkerPlane.dat", "Fokker DR 1", 
+                "Germany", 'D', 'A', 1000.00, 13);
+            fokkerPlane.WriteBinary();
+            fokkerPlane.ReadBinary();
+            Console.WriteLine();
+
+            Plane spadPlane = new Plane("spadPlane.dat", "SPAD XIII", 
+                "France", 'A', 'A', 1250.50, 16);
+            spadPlane.WriteBinary();
+            spadPlane.ReadBinary();
+            Console.WriteLine();
+
+            Plane sopwithPlane = new Plane("sopwithPlane.dat", "Sopwith Camel", 
+                "England", 'C', 'A', 890.00, 15);
+            sopwithPlane.WriteBinary();
+            sopwithPlane.ReadBinary();
+            Console.WriteLine();
+
+            Plane albatrosPlane = new Plane("albatrosPlane.dat", "Albatros D", 
+                "Germany", 'B', 'A', 1575.75, 15);
+            albatrosPlane.WriteBinary();
+            albatrosPlane.ReadBinary();
+            Console.WriteLine();
+
+            
+
+        }
+    }
+}
